77aeeee555049db17c3c759db8e5145009c75185
Kangyelim1 <gyelim887@gmail.com>
2025-06-08 23:21:30 +0900

    0608 1

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isGiant = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"충돌 발생: {collision.gameObject.name}, 태그: {collision.gameObject.tag}");
+
+
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Finish 지점 도착! 다음 레벨로 이동 시도.");
+
+            LevelObject levelObject = collision.gameObject.GetComponent<LevelObject>();
+
+            // LevelObject 컴포넌트가 있는지 확인합니다.
+            if (levelObject != null)
+            {
+                levelObject.MoveTonextLevel();
+                Debug.Log("MoveToNextLevel() 호출 완료. 다음 씬으로 넘어갈 예정.");
+            }
+            else
+            {
+                Debug.LogWarning("경고: 'Finish' 태그를 가진 오브젝트에 LevelObject 컴포넌트가 없습니다. 다음 스테이지로 넘어갈 수 없습니다.");
+            }
+        }
+        
+    }
+
+}
+
